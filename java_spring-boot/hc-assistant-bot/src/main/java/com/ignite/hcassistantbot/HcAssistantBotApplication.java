8f9621c2b20bf72a75b205088e62dd40d18c34b9
Daniel Padilla <padilladaniel777@gmail.com>
2022-06-29 13:15:24 -0500

    React and springboot skeleton

---
+++ java_spring-boot/hc-assistant-bot/src/main/java/com/ignite/hcassistantbot/HcAssistantBotApplication.java
@@ -0,0 +1,13 @@
+package com.ignite.hcassistantbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcAssistantBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcAssistantBotApplication.class, args);
+	}
+
+}
---
+++ java_spring-boot/hc-assistant-bot/src/test/java/com/ignite/hcassistantbot/HcAssistantBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignite.hcassistantbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcAssistantBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
